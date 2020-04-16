5f9a6f82ef7f56a5fe7122222ebefa48a0946693
roopvirk6 <virkikroop13@gmail.com>
2020-04-16 17:57:39 -0600

    [ikroop] function updated

--- app/src/main/java/com/example/finalproject_mad3125/Singleton.java
+++ app/src/main/java/com/example/finalproject_mad3125/Singleton.java
@@ -13,7 +13,7 @@ public class Singleton
 
       // Customer c1 = new Customer(1,"roop","virk","roop@gmail.com",1000,,"mobile");
         Mobile m1 = new Mobile(1,"02-01-2020","Mobile",1000.0,"rogers","calling+talktime",12455,10,60);
-       // c1.setBill(m1);
+        c1.setBill(m1);
 
         Customer c2 = new Customer(2,"monica","sharma","monica@gmail.com",2000,,"Internet");
         Internet i1 = new Internet(2,"03-01-2020","Internet",300.0,"bell",56);
