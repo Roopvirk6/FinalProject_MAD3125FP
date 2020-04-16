aa25943de7b61d491cc858357bf24d2b24942f5d
roopvirk6 <virkikroop13@gmail.com>
2020-04-16 16:42:19 -0600

    [ikroop] adding bills

--- app/src/main/java/com/example/finalproject_mad3125/Singleton.java
+++ app/src/main/java/com/example/finalproject_mad3125/Singleton.java
@@ -19,7 +19,7 @@ public class Singleton
         Internet i1 = new Internet(2,"03-01-2020","Internet",300.0,"bell",56);
          c2.setBill(i1);
 
-         Customer c3 = new Customer();
+         Customer c3 = new Customer(3,"shilpa","sharma","shilpa@gmail.com",300,,"Hydro");
 
     }
 }
