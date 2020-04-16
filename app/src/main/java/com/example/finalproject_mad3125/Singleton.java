b15bfeee0d4ea6ee71649dffcdb4f26bd12f49ca
roopvirk6 <virkikroop13@gmail.com>
2020-04-16 16:27:57 -0600

    [ikroop] class updated

--- app/src/main/java/com/example/finalproject_mad3125/Singleton.java
+++ app/src/main/java/com/example/finalproject_mad3125/Singleton.java
@@ -10,7 +10,9 @@ public class Singleton
     private Singleton()
     {
         arrayListCustomer = new ArrayList<>();
-       // Customer c1 = new Customer(1,"roop","virk","roop@gmail.com",1000,,"mobile");
+       Customer c1 = new Customer(1,"roop","virk","roop@gmail.com",1000,,"mobile");
         Mobile m1 = new Mobile(1,"02-01-2020","Mobile",1000.0,"rogers","calling+talktime",12455,10,60);
+        c1.setBill(m1);
+        
     }
 }
