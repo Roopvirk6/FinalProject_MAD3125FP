126805f22b0c576158326831120c2456b1912e67
roopvirk6 <virkikroop13@gmail.com>
2020-04-16 16:13:50 -0600

    [ikroop] showdetails modified with populatedata

--- app/src/main/java/com/example/finalproject_mad3125/ui/ShowDetails/ShowDetailsFragment.java
+++ app/src/main/java/com/example/finalproject_mad3125/ui/ShowDetails/ShowDetailsFragment.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
@@ -39,6 +40,7 @@ public class ShowDetailsFragment extends Fragment {
     {
         populateData();
         demoAdapter = new CustomerListAdapter(customerList);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getContext());
 
     }
 
