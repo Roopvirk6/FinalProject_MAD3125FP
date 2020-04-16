50a522b93efb1630a6ca68d7e24ed53779920b93
roopvirk6 <virkikroop13@gmail.com>
2020-04-16 15:13:59 -0600

    [ikroop] modified

--- app/src/main/java/com/example/finalproject_mad3125/ui/ShowDetails/ShowDetailsFragment.java
+++ app/src/main/java/com/example/finalproject_mad3125/ui/ShowDetails/ShowDetailsFragment.java
@@ -7,14 +7,21 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.finalproject_mad3125.Customer;
 import com.example.finalproject_mad3125.R;
 
+import java.util.List;
+
 public class ShowDetailsFragment extends Fragment {
+
+    private RecyclerView emplistrv;
+    private List<Customer> customerList;
 
     private ShowDetailsViewModel mViewModel;
 
