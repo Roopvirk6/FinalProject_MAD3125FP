eb7ea9b3e52b8321b1911d997e762014d8025eb8
roopvirk6 <virkikroop13@gmail.com>
2020-04-16 18:18:51 -0600

    [ikroop] home fragment updated

--- app/src/main/java/com/example/finalproject_mad3125/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/finalproject_mad3125/ui/home/HomeFragment.java
@@ -26,11 +26,11 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+        final TextView dateTextView = root.findViewById(R.id.text_date);
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+                dateTextView.setText(s);
             }
         });
         return root;
