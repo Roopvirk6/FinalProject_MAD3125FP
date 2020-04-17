7d8b391d9bc7bd0b2533435618f992711e17531f
roopvirk6 <virkikroop13@gmail.com>
2020-04-17 16:26:39 -0600

    [ikroop] add customer fragment updated

--- app/src/main/java/com/example/finalproject_mad3125/ui/AddCustomer/GalleryFragment.java
+++ app/src/main/java/com/example/finalproject_mad3125/ui/AddCustomer/GalleryFragment.java
@@ -30,18 +30,16 @@ public class GalleryFragment extends Fragment {
     EditText edtLastName;
     EditText edtEmail;
     EditText edtAmount;
-    RadioGroup rgbBillType;
-    RadioButton rbHydro;
-    RadioButton rbInternet;
-    RadioButton rbMobile;
+    EditText edtBillType;
     Button btnSave;
-    LinearLayout linearBillType;
+
 
     Singleton singleton = Singleton.getInstance();
     String firstname;
     String lastName;
     String custID;
     String email;
+    String btype;
     double amount;
     Bill bill = null;
     Customer customer = null;
@@ -56,34 +54,11 @@ public class GalleryFragment extends Fragment {
         edtLastName = root.findViewById(R.id.edtLastName);
         edtEmail = root.findViewById(R.id.edtEmail);
         edtAmount = root.findViewById(R.id.edtAmount);
+        edtBillType = root.findViewById(R.id.edtBillType);
         btnSave = root.findViewById(R.id.btnSave);
-        rgbBillType = root.findViewById(R.id.rgbBillType);
-        rbHydro = root.findViewById(R.id.rbHydro);
-        rbInternet = root.findViewById(R.id.rbInternet);
-        rbMobile = root.findViewById(R.id.rbMobile);
-        linearBillType = root.findViewById(R.id.linearBillType);
-        rgbBillType.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup radioGroup, int i) {
 
 
-                switch (i) {
-                    case R.id.rbHydro:
 
-                        break;
-
-                    case R.id.rbInternet:
-
-
-                        break;
-
-                    case R.id.rbMobile:
-
-                        break;
-                }
-            }
-
-        });
 
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -104,13 +79,14 @@ public class GalleryFragment extends Fragment {
         custID = edtCustID.getText().toString();
         email=edtEmail.getText().toString();
         amount = Double.parseDouble(edtAmount.getText().toString());
+        btype = edtBillType.getText().toString();
         Customer customer = new Customer();
         customer.setCustomerFirstName(firstname);
         customer.setCustomerLastName(lastName);
         customer.setCustomerId(custID);
         customer.setCustomerEmailAddress(email);
         customer.setAmount(amount);
-        addBillData(customer);
+        customer.setBillType(btype);
         singleton.addCustomer(customer);
         return customer;
 
@@ -118,48 +94,6 @@ public class GalleryFragment extends Fragment {
 
     }
 
-
-    public void addBillData(Customer cust)
-    {
-        if (rbHydro.isChecked()){
-
-            Hydro hydro = new Hydro();
-            hydro.setBillID(edtCustID.getText().toString());
-            hydro.setBillType("Hydro");
-            hydro.setBillDate("02-01-2020");
-            hydro.setBillAmount(200.0);
-            cust.setBillType("Hydro");
-            cust.setBill(hydro);
-            bill = hydro;
-
-        }
-
-        if (rbInternet.isChecked())
-        {
-
-            Internet internet = new Internet();
-            internet.setBillID(edtCustID.getText().toString());
-            internet.setBillType("Internet");
-            internet.setBillDate("03-02-2020");
-            internet.setBillAmount(400);
-            cust.setBillType("Internet");
-            cust.setBill(internet);
-            bill = internet;
-
-        }
-        if (rbMobile.isChecked())
-        {
-            Mobile mobile = new Mobile();
-            mobile.setBillID(edtCustID.getText().toString());
-            mobile.setBillType("Mobile");
-            mobile.setBillDate("04-01-2020");
-            mobile.setBillAmount(600.0);
-            cust.setBill(mobile);
-            cust.setBillType("Mobile");
-            bill = mobile;
-        }
-
-    }
 
 
 }
