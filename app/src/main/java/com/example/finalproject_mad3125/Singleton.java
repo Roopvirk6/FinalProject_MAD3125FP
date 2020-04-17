dd885f3bbc2ee523f628a15fff4879c47cf8a40e
roopvirk6 <virkikroop13@gmail.com>
2020-04-16 19:14:34 -0600

    [ikroop] class customer updated

--- app/src/main/java/com/example/finalproject_mad3125/Singleton.java
+++ app/src/main/java/com/example/finalproject_mad3125/Singleton.java
@@ -11,15 +11,15 @@ public class Singleton
     {
         arrayListCustomer = new ArrayList<>();
 
-      Customer c1 = new Customer(1,"roop","virk","roop@gmail.com",1000,,"mobile");
+      Customer c1 = new Customer(1,"roop","virk","roop@gmail.com",1000,"mobile");
         Mobile m1 = new Mobile(1,"02-01-2020","Mobile",1000.0,"rogers","calling+talktime",12455,10,60);
         c1.setBill(m1);
 
-        Customer c2 = new Customer(2,"monica","sharma","monica@gmail.com",2000,,"Internet");
+        Customer c2 = new Customer(2,"monica","sharma","monica@gmail.com",2000,"Internet");
         Internet i1 = new Internet(2,"03-01-2020","Internet",300.0,"bell",56);
          c2.setBill(i1);
 
-         Customer c3 = new Customer(3,"shilpa","sharma","shilpa@gmail.com",300,,"Hydro");
+         Customer c3 = new Customer(3,"shilpa","sharma","shilpa@gmail.com",300,"Hydro");
          Hydro h1 = new Hydro(3,"03-06-2020","Hydro",450.0,"torontohydro",200);
          c3.setBill(h1);
 
