acd524666c6b15a799aefd70ee301dfbd1a22106
roopvirk6 <virkikroop13@gmail.com>
2020-04-18 14:11:55 -0600

    [ikroop] add customer fragment updated

--- app/src/main/java/com/example/finalproject_mad3125/Singleton.java
+++ app/src/main/java/com/example/finalproject_mad3125/Singleton.java
@@ -11,17 +11,21 @@ public class Singleton
     {
         arrayListCustomer = new ArrayList<>();
 
-      Customer c1 = new Customer(1,"roop","virk","roop@gmail.com",1000,"mobile");
-        Mobile m1 = new Mobile(1,"02-01-2020","Mobile",1000.0,"rogers","calling+talktime",12455,10,60);
+      Customer c1 = new Customer("1","roop","virk","roop@gmail.com",1000.0,"mobile");
+        Mobile m1 = new Mobile("1","02-01-2020","Mobile",1000.0,"rogers","calling+talktime","12345",10,60);
         c1.setBill(m1);
+        addCustomer(c1);
 
-        Customer c2 = new Customer(2,"monica","sharma","monica@gmail.com",2000,"Internet");
-        Internet i1 = new Internet(2,"03-01-2020","Internet",300.0,"bell",56);
+
+        Customer c2 = new Customer("2","monica","sharma","monica@gmail.com",2000,"Internet");
+        Internet i1 = new Internet("2","03-01-2020","Internet",300.0,"bell",56);
          c2.setBill(i1);
+         addCustomer(c2);
 
-         Customer c3 = new Customer(3,"shilpa","sharma","shilpa@gmail.com",300,"Hydro");
-         Hydro h1 = new Hydro(3,"03-06-2020","Hydro",450.0,"torontohydro",200);
+         Customer c3 = new Customer("3","shilpa","sharma","shilpa@gmail.com",300,"Hydro");
+         Hydro h1 = new Hydro("3","03-06-2020","Hydro",450.0,"torontohydro",200);
          c3.setBill(h1);
+         addCustomer(c3);
 
     }
 
