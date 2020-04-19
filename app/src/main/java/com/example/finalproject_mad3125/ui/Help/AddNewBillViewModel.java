9127c48e47b281a0503bb9525e5575328344c988
roopvirk6 <virkikroop13@gmail.com>
2020-04-19 12:28:44 -0600

    [ikroop] view model for help

--- app/src/main/java/com/example/finalproject_mad3125/ui/Help/AddNewBillViewModel.java
+++ app/src/main/java/com/example/finalproject_mad3125/ui/Help/AddNewBillViewModel.java
@@ -1,7 +1,20 @@
 package com.example.finalproject_mad3125.ui.Help;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class AddNewBillViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public AddNewBillViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is tools fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
     // TODO: Implement the ViewModel
 }
