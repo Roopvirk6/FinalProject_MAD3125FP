4331f1f96df850e0711a0d0e04070f0388abbe7a
roopvirk6 <virkikroop13@gmail.com>
2020-04-19 13:51:45 -0600

    [ikroop] Customerlist item

--- app/src/main/java/com/example/finalproject_mad3125/CustomerList/CustomerListItem.java
+++ app/src/main/java/com/example/finalproject_mad3125/CustomerList/CustomerListItem.java
@@ -2,5 +2,29 @@ package com.example.finalproject_mad3125.CustomerList;
 
 public class CustomerListItem
 {
-    
+    private String customerFirstName;
+    private String customerEmailAddress;
+
+    public CustomerListItem(String customerFirstName, String customerEmailAddress)
+    {
+        super();
+        this.customerFirstName = customerFirstName;
+        this.customerEmailAddress = customerEmailAddress;
+    }
+
+    public String getCustomerFirstName() {
+        return customerFirstName;
+    }
+
+    public void setCustomerFirstName(String customerFirstName) {
+        this.customerFirstName = customerFirstName;
+    }
+
+    public String getCustomerEmailAddress() {
+        return customerEmailAddress;
+    }
+
+    public void setCustomerEmailAddress(String customerEmailAddress) {
+        this.customerEmailAddress = customerEmailAddress;
+    }
 }
